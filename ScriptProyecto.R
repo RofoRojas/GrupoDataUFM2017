005ea76bc1f0480333e4fcd43b0df34d95e46922
jimenasr <33358235+jimenasr@users.noreply.github.com>
2017-11-22 23:11:53 -0600

    Update ScriptProyecto.R

--- ScriptProyecto.R
+++ ScriptProyecto.R
@@ -55,7 +55,7 @@ datasetfinal$Fecha <- dmy(datasetfinal$Fecha)
 datasetfinal <- datasetfinal %>% 
   mutate(Mes=month(Fecha, label = T, abbr=T ), Dia.Semana = wday(Fecha, label = T, abbr=T), No.Dia.Mes= mday(Fecha), No.Dia.Year = yday(Fecha), No.Semana = isoweek(Fecha))
 
-#resumenes
+#resumenes por tienda, departamento, marca, proveedor y mes
 resumen <- datasetfinal %>% 
   group_by(Tienda, Proveedor, Marca, Departamento, Mes) %>%
   summarise(Ventas=sum(Ventas))
@@ -73,6 +73,8 @@ resumenprov <- datasetfinal %>%
 
 resumentienda <- aggregate(Ventas~Tienda, data=resumen, sum) 
 
+# GRÁFICAS
+# Permiten entender más fácilmente el dataset
 #Pie de Tiendas
 rt <- plot_ly(resumentienda, labels = ~Tienda, values = ~Ventas, type = 'pie',
               textposition = 'inside',
@@ -84,6 +86,7 @@ rt <- plot_ly(resumentienda, labels = ~Tienda, values = ~Ventas, type = 'pie',
          yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))
 rt
 ##a partir de esta grafica nos enfocaremos en la distribucion unicamente para Despensa Familiar
+##la maxidespensa no tiene relevancia en el dataset
 
 
 #Pie de Marcas, Error por UTF8
@@ -95,8 +98,8 @@ rm <- plot_ly(resumenbrand, labels = ~Marca, values = ~Ventas, type = 'pie',
   layout(title = 'Ventas por Marca',
          xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
          yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))
-
 rm
+##son importantes saba, nevax, snickers, m&m's y vanish
 
 #Pie de Proveedores, Error por UTF8
 rp <- plot_ly(resumenprov, labels = ~Proveedor, values = ~Ventas, type = 'pie',
@@ -107,8 +110,8 @@ rp <- plot_ly(resumenprov, labels = ~Proveedor, values = ~Ventas, type = 'pie',
   layout(title = 'Ventas por Marca',
          xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
          yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))
-
 rp
+##son importantes mars y sca
 
 #Ventas por dia en la Despensa Familiar
 p<- datasetfinal %>% 
@@ -117,9 +120,25 @@ p<- datasetfinal %>%
   summarise(Ventas=sum(Ventas))%>%
   plot_ly(x = ~Fecha, y = ~Ventas, colors = "Dark2", mode = 'lines', type = 'scatter')%>%
   layout(title = 'Ventas de Despensa Familiar')
-
 p
 
+#Ventas por dia del Mes de Despensa por dia del mes
+d<- datasetfinal %>% 
+  filter(Tienda== "Despensa Familiar", Proveedor!="&Caf<U+008E>") %>%
+  group_by(Marca, Dia.Semana) %>% 
+  summarise(Ventas=sum(Ventas))%>%
+  plot_ly(x = ~Dia.Semana, y = ~Ventas, color= ~Marca, colors = "Paired", type = 'bar')%>%
+  layout(title = 'Ventas de Despensa por dia de la Semana')
+d
+
+#Ventas por dia del Mes de Despensa por dia del mes
+c<- datasetfinal %>% 
+  filter(Tienda== "Despensa Familiar", Proveedor!="&Caf<U+008E>") %>%
+  group_by(Marca, No.Dia.Mes) %>% 
+  summarise(Ventas=sum(Ventas))%>%
+  plot_ly(x = ~No.Dia.Mes, y = ~Ventas, color= ~Marca, colors = "Paired", type = 'bar')%>%
+  layout(title = 'Ventas de Despensa por dia del mes')
+c
 
 #Ventas por dia del Mes de SCA por dia del mes
 a<- datasetfinal %>% 
@@ -130,25 +149,6 @@ a<- datasetfinal %>%
   layout(title = 'Ventas de SCA por dia del mes')
 a
 
-#Ventas por dia del Mes de DEspensa por dia del mes
-c<- datasetfinal %>% 
-  filter(Tienda== "Despensa Familiar", Proveedor!="&Caf<U+008E>") %>%
-  group_by(Marca, No.Dia.Mes) %>% 
-  summarise(Ventas=sum(Ventas))%>%
-  plot_ly(x = ~No.Dia.Mes, y = ~Ventas, color= ~Marca, colors = "Paired", type = 'bar')%>%
-  layout(title = 'Ventas de Despensa por dia del mes')
-c
-
-
-#Ventas por dia del Mes de DEspensa por dia del mes
-d<- datasetfinal %>% 
-  filter(Tienda== "Despensa Familiar", Proveedor!="&Caf<U+008E>") %>%
-  group_by(Marca, Dia.Semana) %>% 
-  summarise(Ventas=sum(Ventas))%>%
-  plot_ly(x = ~Dia.Semana, y = ~Ventas, color= ~Marca, colors = "Paired", type = 'bar')%>%
-  layout(title = 'Ventas de Despensa por dia de la Semana')
-d
-
 #Ventas por dia del Mes Mars por dia del mes
 b<- datasetfinal %>% 
   filter(Tienda== "Despensa Familiar", Proveedor=="Mars") %>%
@@ -158,8 +158,10 @@ b<- datasetfinal %>%
   layout(title = 'Ventas de Mars por dia del mes')
 b
 
+
 #A partir de las graficas anteriores decidimos enfocarnos en el Proveedor Mars para la Tienda Despensa Familiar
 #Tanto por su importancia en cantidades vendidas como en su diversidad
+#Tienen varias marcas y están presentes en tres áreas (impulse, cookies and candies y pets)
 
 #Dataset especifico de Mars 
 datasetMars<-datasetfinal %>% 
